c0e171cd1910bb1d89e23b51c23d073f9c96874c
Kongyot Rojphutthikul <kongtoonarmy@hotmail.com>
2015-09-17 23:05:51 +0700

    Update store(), show() of AccountCardController

--- app/Http/Controllers/AccountCardController.php
+++ app/Http/Controllers/AccountCardController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Illuminate\Http\Response;
+
 use Stripe\Stripe as Stripe;
 use Stripe\Account as StripeAccount;
 use Stripe\Card as StripeCard;
@@ -43,9 +45,36 @@ class AccountCardController extends Controller
      * @param  Request  $request
      * @return Response
      */
-    public function store(Request $request)
+    public function store($accoutId)
     {
-        //
+        Stripe::setApiKey(\Config::get('stripe.key'));
+
+        try {
+
+            $stripeToken = self::getStripeToken();
+
+            $stripeAccount = StripeAccount::retrieve($accoutId);  
+
+            //$stripeAccount->external_accounts->create(['external_account' => $stripeToken]); 
+
+            //$stripeAccount->bank_accounts->create(['external_account' => $stripeToken]); 
+
+            StripeCharge::create([
+              "amount" => 400,
+              "currency" => "usd",
+              "customer" => $accoutId,
+              "source" => 'card_16m4c7GXcJiLbfUVJE9Yibvn',
+              "description" => "Charge for test@example.com"
+            ]);    
+
+            return response()->json($stripeAccount);
+
+            
+
+        } catch (StripeError\Base $e) {
+
+            return $e->getMessage();
+        } 
     }
 
     /**
@@ -64,36 +93,9 @@ class AccountCardController extends Controller
             $account = StripeAccount::retrieve($accountId);
             $card = $account->external_accounts->retrieve($cardId);
 
-            $stripeCustomer = StripeCustomer::create([
-              "email" => 'email@example.com',
-              "description" => "Customer for test@example.com",
-              "source" => self::getStripeToken()
-            ]);
-            echo 'customerID = '.$stripeCustomer->id;
-
-            /*$stripeToken = StripeToken::create($card);
-            echo 'token = '.$stripeToken.'<br>';*/
-
-            /*$stripeToken = StripeToken::create([
-                "card" => [
-                    "number" => "5200828282828210",
-                    "exp_month" => 8,
-                    "exp_year" => 2018,
-                    "cvc" => "314",
-                    "currency" => "usd"
-                ]
-            ]);
-            echo 'token = '.$stripeToken.'<br>';*/
             
 
-            /*StripeCharge::create(array(
-              "amount" => 400,
-              "currency" => "usd",
-              "customer" => $stripeCustomer->id,
-              "description" => "Charge for test@example.com"
-            ));*/
-
-            return view('accounts.cards.show', ['card' => $card]);    
+            //return view('accounts.cards.show', ['card' => $card]);    
 
         } catch (StripeError\Base $e) {
 
@@ -143,9 +145,9 @@ class AccountCardController extends Controller
 
             $stripeToken = StripeToken::create([
                 "card" => [
-                    "number" => "5200828282828210",
-                    "exp_month" => 8,
-                    "exp_year" => 2018,
+                    "number" => "4000056655665556",
+                    "exp_month" => 12,
+                    "exp_year" => 2016,
                     "cvc" => "314",
                     "currency" => "usd"
                 ]
@@ -157,5 +159,53 @@ class AccountCardController extends Controller
 
             return $this->sendBackJsonError($e->getHttpStatus());
         }
+    }
+
+    public function temp($accountId, $cardId)
+    {
+
+        Stripe::setApiKey(\Config::get('stripe.key'));
+
+        try {
+
+            $account = StripeAccount::retrieve($accountId);
+            $card = $account->external_accounts->retrieve($cardId);
+
+            $stripeCustomer = StripeCustomer::create([
+              "email" => 'email@example.com',
+              "description" => "Customer for test@example.com",
+              "source" => self::getStripeToken()
+            ]);
+            echo 'customerID = '.$stripeCustomer->id;
+
+            /*$stripeToken = StripeToken::create($card);
+            echo 'token = '.$stripeToken.'<br>';*/
+
+            /*$stripeToken = StripeToken::create([
+                "card" => [
+                    "number" => "5200828282828210",
+                    "exp_month" => 8,
+                    "exp_year" => 2018,
+                    "cvc" => "314",
+                    "currency" => "usd"
+                ]
+            ]);
+            echo 'token = '.$stripeToken.'<br>';*/
+            
+
+            StripeCharge::create(array(
+              "amount" => 400,
+              "currency" => "usd",
+              "customer" => $stripeCustomer->id,
+              "source" => '',
+              "description" => "Charge for test@example.com"
+            ));
+
+            return view('accounts.cards.show', ['card' => $card]);    
+
+        } catch (StripeError\Base $e) {
+
+            return $e->getMessage();
+        }     
     }
 }
